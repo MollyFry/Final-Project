fcc9df2b4f97da6a782628cd5051e621a8f0290b
MollyFry <frym@union.edu>
2025-03-11 14:38:28 -0400

    Create museum_security_system.h

---
+++ modules/museum_security_system/museum_security_system.h
@@ -0,0 +1,14 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _MUSEUM_SECURITY_SYSTEM_H_
+#define _MUSEUM_SECURITY_SYSTEM_H_
+
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void museum_security_systemInit();
+void museum_security_systemUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif // _MUSEUM_SECURITY_SYSTEM_H_
