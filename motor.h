22249e90976b44e754aa9f88985654d6a9216dd7
MollyFry <frym@union.edu>
2025-03-11 14:21:15 -0400

    Create motor.h

---
+++ motor.h
@@ -0,0 +1,19 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _MOTOR_H_
+#define _MOTOR_H_
+
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+
+void motorControlInit();
+
+
+void motorControlUpdate();
+
+
+
+//=====[#include guards - end]=================================================
+
+#endif // _MOTOR_H_
