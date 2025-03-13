768b28ffd761e2fc196c0daf4be5be86104a946c
MollyFry <frym@union.edu>
2025-03-13 09:53:40 -0400

    Update motor.cpp

--- modules/motor/motor.cpp
+++ modules/motor/motor.cpp
@@ -11,12 +11,11 @@
 //=====[Declaration and initialization of public global objects]===============
 
 
-
-DigitalInOut motorM1Pin(PF_2);
+//defining the pins for the motor
+DigitalInOut motorM1Pin(PF_2); 
 DigitalInOut motorM2Pin(PE_6);
 
-DigitalIn resetButton(PF_9);
-
+//defining a variable to be enumerated
 int motorUpdateCounter;
 
 
@@ -25,7 +24,7 @@ int motorUpdateCounter;
 
 
 
-void motorControlInit()
+void motorControlInit() //initialize the motor
 {
     motorM1Pin.mode(OpenDrain);
     motorM2Pin.mode(OpenDrain);
@@ -33,8 +32,7 @@ void motorControlInit()
     motorM1Pin.input();
     motorM2Pin.input();
     motorUpdateCounter = 0;
-    motorUpdateCounter2 = 0;
-    resetButton.mode(PullDown);
+
 
 
 }
@@ -42,15 +40,15 @@ void motorControlInit()
 
 
 
-void motorControlUpdate()
+void motorControlUpdate() //this makes the motor run when it's called
 {   
-    if ( (motorUpdateCounter <= 30)) { 
+    if ( (motorUpdateCounter <= 30)) { //makes sure that the motor only runs for a set amount of time
             motorM2Pin.input(); //swap input and output to change direction!!
             motorM1Pin.output();
             motorUpdateCounter++;
 
     if (motorUpdateCounter > 30){ //change number to determine how long it runs for
-        motorM2Pin.input();
+        motorM2Pin.input(); //setting both of these pins to input stops the motor
         motorM1Pin.input();
         }
 
